3004c8ababb2967c3f0a5104fbd2f7a81a7fb50b
Garv Kumar <91613089+garvboiiiii@users.noreply.github.com>
2025-07-30 13:53:12 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,5 +1,5 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "your-bot-token")
-WEBHOOK_URL = os.getenv("WEBHOOK_URL", "https://your.webhook.url")
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:pass@localhost/db")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
+DATABASE_URL = os.getenv("DATABASE_URL")
